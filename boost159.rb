9ab293341248aaf5aa87b0acff3966688140cf1e
t3sting3 <xvgtwitter@protonmail.com>
2024-10-23 13:17:50 -0400

    Update boost159.rb

--- boost159.rb
+++ boost159.rb
@@ -1,4 +1,4 @@
-class BoostAT159 < Formula
+class Boost159 < Formula
   desc "Collection of portable C++ source libraries"
   homepage "https://www.boost.org"
   url "https://downloads.sourceforge.net/project/boost/boost/1.59.0/boost_1_59_0.tar.bz2"
