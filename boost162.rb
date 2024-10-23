27b1984d2a11819a505a457398b1b8adc008d5b5
t3sting3 <xvgtwitter@protonmail.com>
2024-10-22 21:39:04 -0400

    Update and rename boost167.rb to boost162.rb

--- boost162.rb
+++ boost162.rb
@@ -1,36 +1,51 @@
-
 class Boost < Formula
   desc "Collection of portable C++ source libraries"
   homepage "https://www.boost.org/"
-  revision 1
+  url "https://downloads.sourceforge.net/project/boost/boost/1.62.0/boost_1_62_0.tar.bz2"
+  sha256 "36c96b0f6155c98404091d8ceb48319a28279ca0333fba1ad8611eb90afb2ca0"
   head "https://github.com/boostorg/boost.git"
 
-  stable do
-    url "https://sourceforge.net/projects/boost/files/boost/1.67.0/boost_1_67_0.tar.bz2"
-    sha256 "2684c972994ee57fc5632e03bf044746f6eb45d4920c343937a465fd67a5adba"
+  env :userpaths
 
-    # Remove for > 1.67.0
-    # Fix "error: no member named 'next' in namespace 'boost'"
-    # Upstream commit from 1 Dec 2017 "Add #include <boost/next_prior.hpp>; no
-    # longer in utility.hpp"
-    patch :p2 do
-      url "https://github.com/boostorg/lockfree/commit/12726cd.patch?full_index=1"
-      sha256 "f165823d961a588b622b20520668b08819eb5fdc08be7894c06edce78026ce0a"
-    end
-  end
-
+  option :universal
   option "with-icu4c", "Build regexp engine with icu support"
   option "without-single", "Disable building single-threading variant"
   option "without-static", "Disable building static library variant"
+  option "with-mpi", "Build with MPI support"
 
   deprecated_option "with-icu" => "with-icu4c"
 
-  depends_on "icu4c" => :optional
+  if build.cxx11?
+    depends_on "icu4c" => [:optional, "c++11"]
+    depends_on "open-mpi" => "c++11" if build.with? "mpi"
+  else
+    depends_on "icu4c" => :optional
+    depends_on :mpi => [:cc, :cxx, :optional]
+  end
+
+  fails_with :llvm do
+    build 2335
+    cause "Dropped arguments to functions when linking with boost"
+  end
+
+  needs :cxx11 if build.cxx11?
 
   def install
+    # https://svn.boost.org/trac/boost/ticket/8841
+    if build.with?("mpi") && build.with?("single")
+      raise <<-EOS.undent
+        Building MPI support for both single and multi-threaded flavors
+        is not supported.  Please use "--with-mpi" together with
+        "--without-single".
+      EOS
+    end
+
+    ENV.universal_binary if build.universal?
+
     # Force boost to compile with the desired compiler
     open("user-config.jam", "a") do |file|
       file.write "using darwin : : #{ENV.cxx} ;\n"
+      file.write "using mpi ;\n" if build.with? "mpi"
     end
 
     # libdir should be set by --prefix but isn't
@@ -44,22 +59,31 @@ class Boost < Formula
     end
 
     # Handle libraries that will not be built.
-    without_libraries = ["python", "mpi"]
+    without_libraries = ["python"]
+
+    # The context library is implemented as x86_64 ASM, so it
+    # won't build on PPC or 32-bit builds
+    # see https://github.com/Homebrew/homebrew/issues/17646
+    if Hardware::CPU.ppc? || Hardware::CPU.is_32_bit? || build.universal?
+      without_libraries << "context"
+      # The coroutine library depends on the context library.
+      without_libraries << "coroutine"
+    end
 
     # Boost.Log cannot be built using Apple GCC at the moment. Disabled
     # on such systems.
-    without_libraries << "log" if ENV.compiler == :gcc
+    without_libraries << "log" if ENV.compiler == :gcc || ENV.compiler == :llvm
+    without_libraries << "mpi" if build.without? "mpi"
 
     bootstrap_args << "--without-libraries=#{without_libraries.join(",")}"
 
-    # layout should be synchronized with boost-python and boost-mpi
+    # layout should be synchronized with boost-python
     args = ["--prefix=#{prefix}",
             "--libdir=#{lib}",
             "-d2",
             "-j#{ENV.make_jobs}",
             "--layout=tagged",
             "--user-config=user-config.jam",
-            "-sNO_LZMA=1",
             "install"]
 
     if build.with? "single"
@@ -74,11 +98,15 @@ class Boost < Formula
       args << "link=shared"
     end
 
+    args << "address-model=32_64" << "architecture=x86" << "pch=off" if build.universal?
+
     # Trunk starts using "clang++ -x c" to select C compiler which breaks C++11
     # handling using ENV.cxx11. Using "cxxflags" and "linkflags" still works.
-    args << "cxxflags=-std=c++11"
-    if ENV.compiler == :clang
-      args << "cxxflags=-stdlib=libc++" << "linkflags=-stdlib=libc++"
+    if build.cxx11?
+      args << "cxxflags=-std=c++11"
+      if ENV.compiler == :clang
+        args << "cxxflags=-stdlib=libc++" << "linkflags=-stdlib=libc++"
+      end
     end
 
     system "./bootstrap.sh", *bootstrap_args
@@ -91,8 +119,17 @@ class Boost < Formula
     # ENV.compiler doesn't exist in caveats. Check library availability
     # instead.
     if Dir["#{lib}/libboost_log*"].empty?
-      s += <<~EOS
-        Building of Boost.Log is disabled because it requires newer GCC or Clang.
+      s += <<-EOS.undent
+
+      Building of Boost.Log is disabled because it requires newer GCC or Clang.
+      EOS
+    end
+
+    if Hardware::CPU.ppc? || Hardware::CPU.is_32_bit? || build.universal?
+      s += <<-EOS.undent
+
+      Building of Boost.Context and Boost.Coroutine is disabled as they are
+      only supported on x86_64.
       EOS
     end
 
@@ -100,7 +137,7 @@ class Boost < Formula
   end
 
   test do
-    (testpath/"test.cpp").write <<~EOS
+    (testpath/"test.cpp").write <<-EOS.undent
       #include <boost/algorithm/string.hpp>
       #include <string>
       #include <vector>
