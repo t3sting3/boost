4d95ac752f2290305aa5d24c5bbc2ffda6b5b0d5
t3sting3 <xvgtwitter@protonmail.com>
2024-10-23 13:32:33 -0400

    Update boost159.rb

--- boost159.rb
+++ boost159.rb
@@ -1,34 +1,26 @@
-class Boost159 < Formula
+class Boost < Formula
   desc "Collection of portable C++ source libraries"
-  homepage "https://www.boost.org"
-  url "https://downloads.sourceforge.net/project/boost/boost/1.59.0/boost_1_59_0.tar.bz2"
-  sha256 "727a932322d94287b62abb1bd2d41723eec4356a7728909e38adb65ca25241ca"
-  license "BSL-1.0"
+  homepage "https://www.boost.org/"
+  head "https://github.com/boostorg/boost.git"
 
-  keg_only :versioned_formula
+  stable do
+    url "https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2"
+    sha256 "7bcc5caace97baa948931d712ea5f37038dbb1c5d89b43ad4def4ed7cb683332"
 
-  # Fixed compilation of operator<< into a record ostream, when
-  # the operator right hand argument is not directly supported by
-  # formatting_ostream. Fixed https://svn.boost.org/trac/boost/ticket/11549
-  # from https://github.com/boostorg/log/commit/7da193f.patch
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/2ccb6715b3727a5ec4d2582dc8931cd46cbefbf1/boost/boost159-questionable-operator.patch"
-    sha256 "a49fd7461d9f3b478d2bddac19adca93fe0fabab71ee67e8f140cbd7d42d6870"
+    # Remove for > 1.64.0
+    # "Replace boost::serialization::detail::get_data function."
+    # Upstream PR from 26 Jan 2017 https://github.com/boostorg/mpi/pull/39
+    patch :p2 do
+      url "https://github.com/boostorg/mpi/commit/f5bdcc1.patch"
+      sha256 "c7af75a83fef90fdb9858bc988d64ca569ae8d940396b9bc60a57d63fca2587b"
+    end
   end
 
-  # Fixed missing symbols in libboost_log_setup (on mac/clang)
-  # from https://github.com/boostorg/log/commit/870284ed31792708a6139925d00a0aadf46bf09f
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/2ccb6715b3727a5ec4d2582dc8931cd46cbefbf1/boost/boost159-missing-symbols.patch"
-    sha256 "2c3a3bae1691df5f8fce8fbd4e5727d57bd4dd813748b70d7471c855c4f19d1c"
-  end
+  option "with-icu4c", "Build regexp engine with icu support"
+  option "without-single", "Disable building single-threading variant"
+  option "without-static", "Disable building static library variant"
 
-  # Fix build on Xcode 11.4
-  patch do
-    url "https://github.com/boostorg/build/commit/b3a59d265929a213f02a451bb63cea75d668a4d9.patch?full_index=1"
-    sha256 "04a4df38ed9c5a4346fbb50ae4ccc948a1440328beac03cb3586c8e2e241be08"
-    directory "tools/build"
-  end
+  deprecated_option "with-icu" => "with-icu4c"
 
   def install
     # Force boost to compile with the desired compiler
@@ -37,11 +29,14 @@ class Boost159 < Formula
     end
 
     # libdir should be set by --prefix but isn't
-    bootstrap_args = %W[
-      --prefix=#{prefix}
-      --libdir=#{lib}
-      --without-icu
-    ]
+    bootstrap_args = ["--prefix=#{prefix}", "--libdir=#{lib}"]
+
+    if build.with? "icu4c"
+      icu4c_prefix = Formula["icu4c"].opt_prefix
+      bootstrap_args << "--with-icu=#{icu4c_prefix}"
+    else
+      bootstrap_args << "--without-icu"
+    end
 
     # Handle libraries that will not be built.
     without_libraries = ["python", "mpi"]
@@ -52,18 +47,35 @@ class Boost159 < Formula
 
     bootstrap_args << "--without-libraries=#{without_libraries.join(",")}"
 
-    # layout should be synchronized with boost-python
-    args = %W[
-      --prefix=#{prefix}
-      --libdir=#{lib}
-      -d2
-      -j#{ENV.make_jobs}
-      --layout=tagged
-      --user-config=user-config.jam
-      install
-      threading=multi,single
-      link=shared,static
-    ]
+    # layout should be synchronized with boost-python and boost-mpi
+    args = ["--prefix=#{prefix}",
+            "--libdir=#{lib}",
+            "-d2",
+            "-j#{ENV.make_jobs}",
+            "--layout=tagged",
+            "--user-config=user-config.jam",
+            "install"]
+
+    if build.with? "single"
+      args << "threading=multi,single"
+    else
+      args << "threading=multi"
+    end
+
+    if build.with? "static"
+      args << "link=shared,static"
+    else
+      args << "link=shared"
+    end
+
+    # Trunk starts using "clang++ -x c" to select C compiler which breaks C++11
+    # handling using ENV.cxx11. Using "cxxflags" and "linkflags" still works.
+    if build.cxx11?
+      args << "cxxflags=-std=c++11"
+      if ENV.compiler == :clang
+        args << "cxxflags=-stdlib=libc++" << "linkflags=-stdlib=libc++"
+      end
+    end
 
     system "./bootstrap.sh", *bootstrap_args
     system "./b2", "headers"
@@ -75,8 +87,9 @@ class Boost159 < Formula
     # ENV.compiler doesn't exist in caveats. Check library availability
     # instead.
     if Dir["#{lib}/libboost_log*"].empty?
-      s += <<~EOS
-        Building of Boost.Log is disabled because it requires newer GCC or Clang.
+      s += <<-EOS.undent
+
+      Building of Boost.Log is disabled because it requires newer GCC or Clang.
       EOS
     end
 
@@ -84,13 +97,14 @@ class Boost159 < Formula
   end
 
   test do
-    (testpath/"test.cpp").write <<~EOS
+    (testpath/"test.cpp").write <<-EOS.undent
       #include <boost/algorithm/string.hpp>
       #include <string>
       #include <vector>
       #include <assert.h>
       using namespace boost::algorithm;
       using namespace std;
+
       int main()
       {
         string str("a,b");
@@ -102,8 +116,7 @@ class Boost159 < Formula
         return 0;
       }
     EOS
-    system ENV.cxx, "test.cpp", "-std=c++1y", "-I#{include}", "-L#{lib}",
-                                "-lboost_system", "-o", "test"
+    system ENV.cxx, "test.cpp", "-std=c++1y", "-L#{lib}", "-lboost_system", "-o", "test"
     system "./test"
   end
 end
